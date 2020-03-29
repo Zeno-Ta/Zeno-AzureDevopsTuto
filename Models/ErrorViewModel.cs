52c4211a888b3a1e88e8ce470241b23804f5c5cf
Zeno-Ta <tanekengt@yahoo.fr>
2020-03-29 16:19:56 -0400

    Creation du projet

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Zeno_AzureDevopsTuto.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
